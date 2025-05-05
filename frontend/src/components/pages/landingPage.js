e52ae2e370466d4a966dfaa70ad66a11f5bcc407
victor mazo <victordmazo@gmail.com>
2025-05-05 17:30:22 -0400

    added image and added conditional for whether login functions appear

--- frontend/src/components/pages/landingPage.js
+++ frontend/src/components/pages/landingPage.js
@@ -1,27 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
 import Card from 'react-bootstrap/Card';
+import getUserInfo from '../../utilities/decodeJwt';
 
 const Landingpage = () => {
-    
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        setUser(getUserInfo());
+    }, []);
+
     return (
-        <div className="bg-blue-500 text-white p-5 h-screen flex justify-center items-center">
-            <Card style={{ width: '30rem', height: '30rem' }} className="mx-2 my-2">
+        <div
+            style={{
+                backgroundImage: 'url("/74131098007-mbta-train-5.jpg")',
+                backgroundSize: 'cover',
+                backgroundPosition: 'center',
+                height: '100vh',
+            }}
+            className="text-white p-5 flex justify-center items-center"
+        >
+            <Card
+                style={{
+                    width: '30rem',
+                    height: '30rem',
+                    backgroundColor: 'rgba(0, 0, 0, 0.6)',
+                    color: 'white',
+                }}
+                className="mx-2 my-2"
+            >
                 <Card.Body className="text-center">
-                <div className="text-4xl font-bold mb-5">MBTA Commuter Rail Explorer</div>
+                    <div className="text-4xl font-bold mb-5">MBTA Commuter Rail Explorer</div>
                     <div className="text-2xl mb-10">Discover MA on the commuter rail!</div>
-                    <div className='mb-4'>
-                    <Card.Link href="/signup" className="flex justify-center items-center justify-center items-center  text-3xl">Sign Up</Card.Link>
-                    </div>
-                    <div className='mb-4'>
-                    <Card.Link href="/login" className="flex justify-center items-center justify-center items-center text-3xl text-blue-30" >Login</Card.Link>
-                    </div>
-                    <div>
-                    <Card.Link href="/tripPlannerPage" className="flex justify-center items-center justify-center items-center text-3xl text-blue-30" >Continue as Guest</Card.Link>
-                    </div>
+
+                    {!user && (
+                        <>
+                            <div className="mb-4">
+                                <Card.Link href="/signup" className="text-3xl text-white">Sign Up</Card.Link>
+                            </div>
+                            <div className="mb-4">
+                                <Card.Link href="/login" className="text-3xl text-white">Login</Card.Link>
+                            </div>
+                            <div>
+                                <Card.Link href="/tripPlannerPage" className="text-3xl text-white">Continue as Guest</Card.Link>
+                            </div>
+                        </>
+                    )}
+
+
                 </Card.Body>
             </Card>
         </div>
-    )
-}
+    );
+};
 
 export default Landingpage;
